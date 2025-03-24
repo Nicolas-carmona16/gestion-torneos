0ae7e4b77afb3603b7d8760cf3c9b2270d99a271
Nicolas <carmonanicolas74mail.com>
2025-03-24 18:12:35 -0500

    feat: implemented field validation and rules in user registration

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,24 @@
 import express from "express";
 import User from "../models/userModel.js";
+import { check } from "express-validator";
 import { registerUser, loginUser } from "../controllers/userController.js";
 import { protect, authorizeRoles } from "../middlewares/authMiddleware.js";
+import validateFields from "../middlewares/validationMiddleware.js";
 
 const router = express.Router();
 
-router.post("/register", registerUser);
+// validations
+const validateRegister = [
+  check("firstName").notEmpty().withMessage("First name is required"),
+  check("lastName").notEmpty().withMessage("Last name is required"),
+  check("email").isEmail().withMessage("Invalid email"),
+  check("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+  validateFields,
+];
+
+router.post("/register", validateRegister, registerUser);
 router.post("/login", loginUser);
 
 // Protect routes
