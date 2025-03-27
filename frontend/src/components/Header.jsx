aacda20b1de0ff0b300ef63c3d3c48ff09609ed3
Nicolas <carmonanicolas74mail.com>
2025-03-27 10:49:05 -0500

    feat: created Home page and Header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,45 @@
+import { AppBar, Toolbar, Button, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
+import logo from "../assets/logoUdeA.png";
+
+const Header = ({ isAuthenticated }) => {
+  return (
+    <AppBar
+      position="static"
+      style={{ backgroundColor: "#026937", height: "100px" }}
+      className="shadow-md"
+    >
+      <Toolbar
+        className="flex justify-between items-center"
+        style={{ minHeight: "100px" }}
+      >
+        <Link to="/" className="flex items-center gap-2">
+          <HomeIcon className="text-white" style={{ fontSize: "32px" }} />
+          <Typography variant="h5" className="text-white ml-2">
+            Torneos UdeA
+          </Typography>
+        </Link>
+        <Link to={"/"}>
+          <img src={logo} alt="Logo" className="h-20" />{" "}
+        </Link>
+        {!isAuthenticated ? (
+          <div>
+            <Button color="inherit" component={Link} to="/login">
+              Login
+            </Button>
+            <Button color="inherit" component={Link} to="/register">
+              Register
+            </Button>
+          </div>
+        ) : (
+          <Button color="inherit" component={Link} to="/logout">
+            Logout
+          </Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { Container, Grid, Button } from "@mui/material";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Link } from "react-router-dom";
+
+const images = [
+  "/images/ganadores1.jpg",
+  "/images/ganadores2.jpg",
+  "/images/ganadores3.jpg",
+];
+
+const Home = () => {
+  return (
+    <Container className="mt-10">
+      <Grid container spacing={3} justifyContent="center" alignItems="center">
+        <Grid
+          item
+          xs={12}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            flexWrap: "wrap",
+            gap: "15px",
+          }}
+        >
+          {[
+            { text: "Inscripciones", path: "/inscripciones" },
+            { text: "Equipos", path: "/equipos" },
+            { text: "Cuadros", path: "/cuadros" },
+            { text: "Programación", path: "/programacion" },
+            { text: "Reglamento", path: "/reglamento" },
+            { text: "Resoluciones", path: "/resoluciones" },
+          ].map(({ text, path }, index) => (
+            <Button
+              key={index}
+              variant="contained"
+              style={{
+                backgroundColor: "#35944b",
+                width: "150px",
+                padding: "10px 0",
+                fontSize: "16px",
+              }}
+              component={Link}
+              to={path}
+            >
+              {text}
+            </Button>
+          ))}
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Carousel
+            showThumbs={false}
+            autoPlay
+            infiniteLoop
+            interval={3000}
+            showStatus={false}
+            style={{ width: "100%", maxWidth: "600px" }}
+          >
+            {images.map((src, index) => (
+              <div key={index}>
+                <img
+                  src={src}
+                  alt={`Imagen ${index + 1}`}
+                  style={{
+                    width: "100%",
+                    maxHeight: "500px",
+                    objectFit: "cover",
+                    borderRadius: "10px",
+                  }}
+                />
+              </div>
+            ))}
+          </Carousel>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default Home;
